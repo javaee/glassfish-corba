33d68300f11c02cb78785719269945020063fa1f
Ken Cavanaugh <Ken.Cavanaugh@sun.com>
2010-02-07 17:28:22 -0800

    Missed a file.

---
+++ test/src/share/classes/corba/tf/TestClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package corba.tf;
+
+/**
+ *
+ * @author ken_admin
+ */
+public interface TestClass {
+    long add( long a, long b ) ;
+
+    long mult( long a, long b ) ;
+}

7e1dc541446cb659780929da221928a4834db79c
hvilekar <devnull@localhost>
2012-12-07 13:43:08 -0800

    add support to build the test from command line

--- iiop-folb-dev-test/samplerun.sh
+++ iiop-folb-dev-test/samplerun.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 # sample local test execution script
 source ./setup.sh
-./run.sh -include "15804sfsb,15804sfsb_kill,15804sfsb_kill_delete"
+#./run.sh 
+./run.sh -include "lbfail,15637,14755"
+#./run.sh -exclude "15804sfsb,15804sfsb_kill,15804sfsb_kill_delete"
 

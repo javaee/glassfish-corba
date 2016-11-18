e6849808f75872b6dab95df389abc420a2d594d9
jwells <devnull@localhost>
2016-11-18 14:58:45 -0500

    Bug 17538883.  Ensure valid permission when creating StubFactoryDynamicBase

--- orbmain/src/main/java/com/sun/corba/ee/impl/presentation/rmi/StubFactoryDynamicBase.java
+++ orbmain/src/main/java/com/sun/corba/ee/impl/presentation/rmi/StubFactoryDynamicBase.java
@@ -42,13 +42,25 @@ package com.sun.corba.ee.impl.presentation.rmi ;
 
 import com.sun.corba.ee.spi.presentation.rmi.PresentationManager ;
 
+import java.io.SerializablePermission;
+
 import com.sun.corba.ee.spi.misc.ORBClassLoader ;
 
 public abstract class StubFactoryDynamicBase extends StubFactoryBase  
 {
     protected final ClassLoader loader ;
+    
+    private static Void checkPermission() {
+        SecurityManager sm = System.getSecurityManager();
+        if (sm != null) {
+            sm.checkPermission(new SerializablePermission(
+                    "enableSubclassImplementation"));
+        }
 
-    public StubFactoryDynamicBase( PresentationManager.ClassData classData, 
+        return null;
+    }
+
+    private StubFactoryDynamicBase( Void unused, PresentationManager.ClassData classData, 
         ClassLoader loader ) 
     {
         super( classData ) ;
@@ -60,6 +72,12 @@ public abstract class StubFactoryDynamicBase extends StubFactoryBase
         } else {
             this.loader = loader ;
         }
+    }
+    
+    public StubFactoryDynamicBase( PresentationManager.ClassData classData, 
+            ClassLoader loader ) 
+    {
+    	this(checkPermission(), classData, loader);
     }
 
     public abstract org.omg.CORBA.Object makeStub() ;
--- orbmain/src/main/java/com/sun/corba/ee/impl/presentation/rmi/proxy/StubFactoryFactoryProxyImpl.java
+++ orbmain/src/main/java/com/sun/corba/ee/impl/presentation/rmi/proxy/StubFactoryFactoryProxyImpl.java
@@ -40,16 +40,28 @@
 
 package com.sun.corba.ee.impl.presentation.rmi.proxy;
 
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
 import com.sun.corba.ee.impl.presentation.rmi.*;
 import com.sun.corba.ee.impl.presentation.rmi.proxy.StubFactoryProxyImpl;
 import com.sun.corba.ee.spi.presentation.rmi.PresentationManager ;
+import com.sun.corba.ee.spi.presentation.rmi.PresentationManager.StubFactory;
 
 public class StubFactoryFactoryProxyImpl extends StubFactoryFactoryDynamicBase 
 {
     public PresentationManager.StubFactory makeDynamicStubFactory( 
-        PresentationManager pm, PresentationManager.ClassData classData, 
-        ClassLoader classLoader ) 
+        PresentationManager pm, final PresentationManager.ClassData classData, 
+        final ClassLoader classLoader ) 
     {
-        return new StubFactoryProxyImpl( classData, classLoader ) ;
+        return AccessController.doPrivileged(
+        		new PrivilegedAction<PresentationManager.StubFactory>() {
+
+					@Override
+					public StubFactory run() {
+						return new StubFactoryProxyImpl( classData, classLoader ) ;
+					}
+        	
+        });
     }
 }
